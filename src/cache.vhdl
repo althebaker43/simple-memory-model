3962e82190f6cce9a9aac45cc4b62e74a2885686
Allen Baker <althebaker43@gmail.com>
2013-04-06 20:52:02 -0500

    Revised cache interface.

--- src/cache.vhdl
+++ src/cache.vhdl
@@ -8,11 +8,23 @@ use IEEE.std_logic_1164.all;
 
 --! @brief Cache entity
 entity cache is
-    port( clk_in : in std_logic;
-          addr_in : in addr;
-          data_in : in word;
-          addr_out : out addr;
-          data_out : out word );
+    port( clk : in std_logic;
+    
+          cpu_addr      : in addr;
+          cpu_data      : inout word;
+          cpu_access    : in std_logic;
+          cpu_write     : in std_logic;
+          cpu_ready     : out std_logic;
+    
+          mem_addr      : out addr;
+          mem_data      : inout word;
+          mem_access    : out std_logic;
+          mem_write     : out std_logic;
+          mem_ready     : in std_logic;
+
+          hit           : out std_logic
+      );
+
 end entity cache;
 
 --! @brief Cache behavioral architecture
--- test/test_cache.vhdl
+++ test/test_cache.vhdl
@@ -5,6 +5,8 @@ use work.datapath_types.all;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.math_real.all;
 
 --! @brief Cache test-bench entity
 entity test_cache is
@@ -14,19 +16,41 @@ end entity test_cache;
 architecture test_cache_arch of test_cache is
 
     signal clk : std_logic;
-    signal addr_in : addr;
-    signal data_in : word;
-    signal addr_out : addr;
-    signal data_out : word;
+    signal clk_en : std_logic;
+    
+    signal cpu_addr     : addr;
+    signal cpu_data     : word;
+    signal cpu_access   : std_logic;
+    signal cpu_write    : std_logic;
+    signal cpu_ready    : std_logic;
+    
+    signal mem_addr     : addr;
+    signal mem_data     : word;
+    signal mem_access   : std_logic;
+    signal mem_write    : std_logic;
+    signal mem_ready    : std_logic;
+
+    signal hit : std_logic;
 
 begin
 
+    clk_gen_ent : entity work.clk_gen( clk_gen_behav )
+        port map( clk,
+                  clk_en );
+
     cache_ent : entity work.cache( cache_behav )
         port map( clk,
-                  addr_in,
-                  data_in,
-                  addr_out,
-                  data_out );
+                  cpu_addr,
+                  cpu_data,
+                  cpu_access,
+                  cpu_write,
+                  cpu_ready,
+                  mem_addr,
+                  mem_data,
+                  mem_access,
+                  mem_write,
+                  mem_ready,
+                  hit );
 
     test : process is
     begin
@@ -34,6 +58,23 @@ begin
         assert false
             report "TEST: Starting cache_behav tests."
             severity note;
+
+        cpu_addr <= NULL_ADDR;
+        cpu_data <= NULL_WORD;
+        cpu_access <= '0';
+        cpu_write <= '0';
+        mem_addr <= NULL_ADDR;
+        mem_data <= NULL_WORD;
+        mem_access <= '0';
+        mem_write <= '0';
+        wait for CLK_PERIOD;
+
+        clk_en <= '1';
+        wait for CLK_PERIOD;
+
+
+        clk_en <= '0';
+        wait for CLK_PERIOD;
         
         assert false
             report "TEST: End of cache_behav tests."
