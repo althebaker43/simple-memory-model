042a234aadc5148893e56742659a4157835ecc46
Allen Baker <althebaker43@gmail.com>
2013-04-07 09:48:17 -0500

    Revised cache lookup procedures.

--- src/cache.vhdl
+++ src/cache.vhdl
@@ -60,18 +60,20 @@ architecture cache_behav of cache is
     --! Cache block type
     type cache_block is array( 0 to 7 ) of word;
 
-    type cache_block_addr is array( 0 to 7 ) of addr;
+    --! Cache block addresses type
+    type cache_block_addrs is array( 0 to 7 ) of addr;
 
     --! Cache block array type
     type block_arr is array( 0 to ( NUM_BLOCKS - 1 ) ) of cache_block;
 
-    type block_addr_arr is array( 0 to ( NUM_BLOCKS - 1 ) ) of cache_block_addr;
+    --! Cache block addresses array type
+    type block_addrs_arr is array( 0 to ( NUM_BLOCKS - 1 ) ) of cache_block_addrs;
 
     --! Cache block availability flag array type
-    type block_arr_avail is array( 0 to ( NUM_BLOCKS - 1 ) ) of bit;
+    type block_avbls_arr is array( 0 to ( NUM_BLOCKS - 1 ) ) of bit;
 
     --! Cache block dirty flag array type
-    type block_arr_dirty is array( 0 to ( NUM_BLOCKS - 1 ) ) of bit;
+    type block_dirtys_arr is array( 0 to ( NUM_BLOCKS - 1 ) ) of bit;
 
 begin
 
@@ -90,74 +92,137 @@ begin
         variable cpu_sample_addr : addr;
         variable cpu_sample_data : word;
 
-        variable addr_present : boolean;
+        variable cur_valid : boolean := false;
+        variable cur_present : boolean := false;
+        variable cur_avbl : boolean := false;
+        variable cur_dirty : boolean := false;
 
         variable mem_sample_data : word;
 
         variable storage : block_arr;
-        variable storage_addr : block_addr_arr;
-        variable storage_avail : block_arr_avail;
-        variable storage_dirty : block_arr_dirty;
+        variable storage_addrs : block_addrs_arr;
+        variable storage_avbls : block_avbls_arr;
+        variable storage_dirtys : block_dirtys_arr;
 
 
-        procedure lookup_addr( sample_addr : in addr;
-                               present : out boolean ) is
-
+        procedure get_block_num( sample_addr : in addr;     --!< Input address to look up
+                                 valid_addr : out boolean;  --!< Indicates if given address is covered by cache
+                                 block_num  : out natural   --!< Block number corresponding to given address (if valid)
+                             ) is
+            
             variable sample_addr_nat : natural;
             variable min_addr_nat : natural;
+            variable max_addr_nat : natural;
             variable abs_addr_nat  : natural;
-            variable block_num : natural;
-            variable cur_block_addr : cache_block_addr;
 
         begin
 
+            valid_addr := false;
+            block_num := 0;
+
             sample_addr_nat := to_integer( sample_addr );
             min_addr_nat := to_integer( min_addr );
+            max_addr_nat := to_integer( max_addr );
+
+            if( ( sample_addr_nat >= min_addr_nat ) and
+                ( sample_addr_nat <= max_addr_nat ) ) then
+                    
+                valid_addr := true;
+
+            end if;
+
             abs_addr_nat := sample_addr_nat - min_addr_nat;
             block_num := abs_addr_nat / BLOCK_MEM_CVRG;
 
-            if storage_avail( block_num ) = '0' then
-                    
-                cur_block_addr := storage_addr( block_num );
-                present := false;
+        end procedure get_block_num;
 
-                for block_addr_indx in cur_block_addr'range loop
 
-                    if sample_addr = cur_block_addr( block_addr_indx ) then
+        procedure query_block( sample_addr  : in addr;      --!< Input address to look up within block
+                               block_num    : in natural;   --!< Index of block to search within
+                               present      : out boolean;  --!< Indicates if given address is present within given block
+                               avbl         : out boolean;  --!< Indicates if given block index is available
+                               dirty        : out boolean;  --!< Indicates if given block is dirty or not
+                               sample_data  : out word      --!< Data at address (if present)
+                           ) is
+            
+            variable cur_block_addrs : cache_block_addrs;
+            variable cur_block : cache_block;
+
+        begin
+
+            present := false;
+            avbl := true;
+            dirty := false;
+            sample_data := NULL_WORD;
+
+            if storage_avbls( block_num ) = '1' then
+
+                avbl := true;
+
+            else
+
+                if storage_dirtys( block_num ) = '1' then
+
+                    dirty := true;
+
+                end if;
+
+                cur_block_addrs := storage_addrs( block_num );
+                cur_block := storage( block_num );
+
+                for block_addr_indx in cur_block_addrs'range loop
+
+                    if sample_addr = cur_block_addrs( block_addr_indx ) then
 
                         present := true;
+                        sample_data := cur_block( block_addr_indx );
 
                     end if;
 
                 end loop;
 
-            else
+            end if;
 
-                present := false;
+        end procedure query_block;
+
+
+        --! Fetches data and block attributes corresponding to given address
+        procedure query_cache( sample_addr  : in addr;      --!< Input address to look up
+                               valid        : out boolean;  --!< Indicates if given address if covered by cache
+                               present      : out boolean;  --!< Indicates if address is present within cache
+                               avbl         : out boolean;  --!< Indicates if corresponding block is available
+                               dirty        : out boolean;  --!< Indicates if corresponding block is dirty
+                               sample_data  : out word      --!< Data at address (if present)
+                          ) is
+
+            variable addr_valid : boolean := false;
+            variable block_num : natural := 0;
+
+        begin
+
+            present := false;
+            valid := false;
+            avbl := true;
+            sample_data := NULL_WORD;
+
+            get_block_num( sample_addr,
+                           addr_valid,
+                           block_num );
+
+            if addr_valid = true then 
+
+                valid := true;
+
+                query_block( sample_addr,
+                             block_num,
+                             present,
+                             avbl,
+                             dirty,
+                             sample_data );
 
             end if;
 
-        end procedure;
-
-
-        --! @todo Convert to procedure
-        function block_avail( sample_addr : in addr ) return boolean is
-
-        begin
-
-            return true;
-
-        end function;
-
-
-        --! @todo Convert to procedure
-        function fetch_data( sample_addr : in addr ) return word is
-
-        begin
-
-            return NULL_WORD;
-
-        end function;
+        end procedure query_cache;
 
 
         procedure store_data( sample_addr : in addr;
@@ -172,7 +237,12 @@ begin
 
             if cpu_read_operation = true then
 
-                lookup_addr( cpu_sample_addr, addr_present );
+                query_cache( cpu_sample_addr,
+                             cur_valid,
+                             cur_present,
+                             cur_avbl,
+                             cur_dirty,
+                             cpu_sample_data );
 
                 if mem_write_operation = true then
 
@@ -192,15 +262,15 @@ begin
 
                     end if;
 
-                elsif addr_present = true then
+                elsif cur_present = true then
 
-                    cpu_data <= fetch_data( cpu_sample_addr );
+                    cpu_data <= cpu_sample_data;
                     cpu_ready <= '1';
                     cpu_read_operation := false;
 
                 else
 
-                    if block_avail( cpu_sample_addr ) = true then
+                    if cur_avbl = true then
 
                         mem_read_operation := true;
 
@@ -215,7 +285,12 @@ begin
 
             elsif cpu_write_operation = true then
 
-                lookup_addr( cpu_sample_addr, addr_present );
+                query_cache( cpu_sample_addr,
+                             cur_valid,
+                             cur_present,
+                             cur_avbl,
+                             cur_dirty,
+                             cpu_sample_data );
 
                 if mem_write_operation = true then
 
@@ -226,7 +301,7 @@ begin
 
                     end if;
 
-                elsif addr_present = true then
+                elsif cur_avbl = true then
 
                     store_data( cpu_sample_addr, cpu_sample_data );
                     cpu_write_operation := false;
